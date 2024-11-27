5d2f2101f13fe189ca5d5e24fa8b2571df988d7c
unknown <copeika.maxim@gmail.com>
2024-11-27 22:23:42 +0200

    Add e2e and screenshot tests

---
+++ e2e-screenshot-tests/tests/screenshots/local.spec.js
@@ -0,0 +1,15 @@
+// tests/screenshots/local.spec.js
+const { test, expect } = require('@playwright/test');
+
+// Тест для перевірки змін сторінки за скриншотом
+test('Перевірка змін сторінки за скриншотом', async ({ page }) => {
+  await page.goto('http://localhost:3000');
+  expect(await page.screenshot()).toMatchSnapshot('screenshots/local-page/index-page.png');
+});
+
+// Тест для порівняння скриншота окремого елемента
+test('Порівняння скриншота елемента', async ({ page }) => {
+  await page.goto('http://localhost:3000');
+  const element = await page.locator('h1');
+  expect(await element.screenshot()).toMatchSnapshot('screenshots/local-page/index-page-element-h1.png');
+});
